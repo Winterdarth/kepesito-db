9253b1eefc332f0c824d39524c9a111a6e40e5fa
David Kajdarov <kajdarov.d@gmail.com>
2021-04-29 12:15:08 +0200

    Finish project

--- src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
+++ src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
@@ -1,5 +1,9 @@
 package hu.nive.ujratervezes.jurassic;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JurassicPark {
 
     private String dbUrl;
@@ -12,4 +16,26 @@ public class JurassicPark {
         this.dbPassword = dbPassword;
     }
 
+    public List<String> checkOverpopulation() {
+        List<String> overpopulated = new ArrayList<>();
+
+        try (Connection connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword)) {
+            String query = "SELECT breed " +
+                    "FROM dinosaur " +
+                    "WHERE expected < actual " +
+                    "ORDER BY breed";
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                if (!overpopulated.contains(resultSet.getString("breed"))) {
+                    overpopulated.add(resultSet.getString("breed"));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return overpopulated;
+    }
 }
